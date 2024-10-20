a6954d2ca92c0933c8149fdf7edd210856744d94
DIEGOACL07 <105613359+DIEGOACL07@users.noreply.github.com>
2024-10-19 20:59:49 -0500

    agregando requerimiento de validaciones

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -41,12 +41,14 @@
                     name="username"
                     type="text"
                     placeholder="Tu Nombre de Usuario"
-                    class="border p-3 w-full rounded-lg"
+                    class="border p-3 w-full rounded-lg @error('username')
+                    border-red-500 @enderror"
+                    value="{{ old($message) }}"
                     >
 
                     @error('username')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
-                @enderror
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
@@ -57,7 +59,9 @@
                     name="email"
                     type="email"
                     placeholder="Tu Email de Registro"
-                    class="border p-3 w-full rounded-lg"
+                    class="border p-3 w-full rounded-lg @error('email')
+                    border-red-500 @enderror"
+                    value="{{ old($message) }}"
                     >
                     @error('email')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
@@ -72,7 +76,8 @@
                     name="password"
                     type="password"
                     placeholder="Tu Password de Registro"
-                    class="border p-3 w-full rounded-lg"
+                    class="border p-3 w-full rounded-lg @error('password')
+                    border-red-500 @enderror"
                     >
                     @error('password')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
